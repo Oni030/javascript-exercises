b5e6547032077cbb280d1825b5fcfce613f69a95
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-14 18:09:25 +0200

    Solution to exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;	
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for(i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let fact = 1;
+  if (x === 0) {
+    return fact = 1;
+  };
+  for(let i = 1; i <= x; i++) {
+    fact *= i;
+  };
+  return fact;
 };
 
 // Do not edit below this line
