33e8aeb330bdb0e4ab3e24503b9a6baf8c2059a8
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-14 18:06:29 +0200

    Solution to exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    };
+    
+    if (typeof start !== 'number' || typeof end !== 'number'){
+        return 'ERROR';
+    };
 
+    let sum = 0;
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        };
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        };
+    }
+    return sum;
 };
+
+console.log(sumAll('x', 10));
 
 // Do not edit below this line
 module.exports = sumAll;
