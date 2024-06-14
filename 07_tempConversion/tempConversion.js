3511b81d0599c087f83d35b9185fa7ffceed50e3
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-14 18:08:48 +0200

    Solution to exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5/9);
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * (9/5) + 32);
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
