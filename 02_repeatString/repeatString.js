f4f04246bf32c80fc0423651d99ec0aa0aa827f0
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-14 18:04:26 +0200

    Solution to exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let concatString = '';
+    if (number < 0) return 'ERROR';
+    for(let i = 0; i < number; i++) {
+        concatString += string;
+    }
+    return concatString;
 };
+
+console.log(repeatString('goodbye', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
