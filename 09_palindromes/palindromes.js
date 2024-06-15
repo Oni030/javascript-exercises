4e7e7fba945cef442431ce03e9a357f9f0d62b5d
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-15 15:29:02 +0200

    Solution to exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = string.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let testString = cleanString;
 
+    while (testString.length >= 0){
+        if(testString[0] !== testString[testString.length - 1]) {
+            return false
+        };
+        if(testString.length <= 1) {
+            return true;
+        };
+        testString = testString.slice(1, testString.length - 1);
+    };
 };
+
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 
 // Do not edit below this line
 module.exports = palindromes;
