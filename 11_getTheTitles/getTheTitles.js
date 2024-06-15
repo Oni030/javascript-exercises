2e1d85b133627ddc8039def9286fdd146fafc6d9
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-15 17:03:05 +0200

    Exercise solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let filteredArr = arr.map((element) => element.title);
+    return filteredArr;
 };
 
 // Do not edit below this line
