511d32ec1810a57a097fd1507871e4d5d8a60cc1
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-16 13:06:15 +0200

    Exercise solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  const currentYear = new Date().getFullYear();
 
+  let oldestPerson = array.reduce((oldest, person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = currentYear;
+    };
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    if (!oldest || personAge > oldest.age) {
+      return { ...person, age: personAge };
+    };
+    return oldest;
+  }, null);
+
+  return oldestPerson
 };
 
 // Do not edit below this line
